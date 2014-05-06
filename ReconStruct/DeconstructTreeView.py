f4cebb2a3b2fc5198fbf672adcf26dabb248d5c0
Sandy Carter <sandy.carter@savoirfairelinux.com>
2014-05-06 12:16:42 -0400

    Add selection highlighting in binary view from deconstruct view

--- ReconStruct/DeconstructTreeView.py
+++ ReconStruct/DeconstructTreeView.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 ReconStruct
 
@@ -19,7 +20,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from PyQt5.QtCore import QItemSelectionModel
+from PyQt5.QtCore import QItemSelectionModel, pyqtSlot
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
 from PyQt5.QtWidgets import QTreeView, QAbstractItemView
 
@@ -57,13 +58,24 @@ class DeconstructTreeView(QTreeView):
         model.setHorizontalHeaderItem(
             self.COL_PREVIEW, QStandardItem(self.tr("Preview")))
 
+        selection_model = QItemSelectionModel(model, self)
+        selection_model.selectionChanged.connect(self.on_selectionChanged)
+
         self.setModel(model)
-        self.setSelectionModel(QItemSelectionModel(self.model(), self))
+        self.setSelectionModel(selection_model)
 
         self.setColumnWidth(self.COL_LABEL, 100)
         self.setColumnWidth(self.COL_SIZE, 50)
         self.setColumnWidth(self.COL_TYPE, 55)
         self.setColumnWidth(self.COL_PREVIEW, 20)
+
+    def on_selectionChanged(self, destination, origin):
+        try:
+            row = destination.indexes()[0].row()
+        except IndexError:
+            row = 0
+        item = self.model().item(row, self.COL_PREVIEW)
+        self.qHexEdit.setSelection(item.start, item.size)
 
     def addRow(self, label="", size="", data_type="", parent=None):
         """
@@ -71,7 +83,7 @@ class DeconstructTreeView(QTreeView):
         :type data_type: str
         """
         if not parent:
-             parent = self.model().invisibleRootItem()
+            parent = self.model().invisibleRootItem()
         if not size:
             # size = hex(self.getSelectionSize())
             size = str(self.getSelectionSize())
@@ -85,10 +97,14 @@ class DeconstructTreeView(QTreeView):
         root = self.model().invisibleRootItem()
         root.removeRows(0, root.rowCount())
         interp, size = self.manifest(bytes(self.qHexEdit.data()))
+        start_index = 0
         for manifest, data in zip(self.manifest, interp[0]):
             parent = root
             preview = QStandardItem(data)
             preview.setEditable(False)
+            preview.size = manifest.size
+            preview.start = start_index
+            start_index += manifest.size
             parent.appendRow([
                 QStandardItem(manifest.label),
                 QStandardItem(manifest._size),
